c17724fdd4a2a58396db12edf9bc98e395d52b17
haohaowoo <B10702211@gapps.ntust.edu.tw>
2021-08-13 15:20:40 +0800

    receive request to delete database

--- main/views.py
+++ main/views.py
@@ -23,17 +23,24 @@ def location(response, location_name):
         return render(response, "main/error_404.html", {})
 
 def create(response):
-    print(response.POST.get('seat',0));
     if response.method == "POST":
+        seat_number=response.POST.get('seat',0)
+        floor = (response.POST.get('location',0))
+        occupy = response.POST.get('occupy',0)
+
+        rows = Seat.objects.filter(seat_number=seat_number)
+        for r in rows: 
+            r.delete() 
+
         s = Seat()
-        floor = Location.objects.get(name=(response.POST.get('location',0)))
-        s.location = floor
-        s.seat_number=response.POST.get('seat',0)
-        s.seat_position_x =10
-        s.seat_position_y =20
-        if response.POST.get('occupy',0) >"0":
+        s.location = Location.objects.get(name=floor)
+        s.seat_number=seat_number
+        s.seat_position_x =0
+        s.seat_position_y =0
+        if occupy >"0":
             s.occupy=True
         else:
             s.occupy=False
         s.save()
+        
     
