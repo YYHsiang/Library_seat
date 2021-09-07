3192b0399ae565c7f9b47efed87b5230d1363e71
YYHsiang <frank183029@gmail.com>
2021-09-07 14:54:49 +0800

    ManyToManyField need to fix

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.base import Model
+from ip_camera.models import Camera
 
 # Create your models here.
 class Location(models.Model):
@@ -7,14 +9,20 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+
+class Camera_Data(models.Model):
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+    occupy = models.BooleanField()
+
 class Seat(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    camera_number = models.IntegerField()
+    camera = models.ManyToManyField(Camera) #every seat is capture by multiple cameras
+    camera_data = models.ManyToManyField(Camera_Data) # data from each camera
+
     seat_number = models.IntegerField()
     seat_position_x = models.FloatField(max_length=200)
     seat_position_y = models.FloatField(max_length=200)
     occupy = models.BooleanField()
-
 
 class Occupy_History(models.Model):
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Location, Seat, Occupy_History
+from .models import Camera_Data, Location, Seat, Occupy_History
+from ip_camera.models import Camera
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
@@ -22,10 +23,68 @@ def create(response):
     if response.method == "POST":
         #obtain request data
         seat_number=response.POST.get('seat',0)
+        camera_name = response.POST.get('camera',0)
         floor = (response.POST.get('location',0))
         occupy = response.POST.get('occupy',0)
 
-        #remove same seat_number
+        seat_temp = Seat.objects.filter(seat_number= seat_number)
+        camera_temp = Camera.objects.filter(name= camera_name)
+        print(seat_temp)
+        print(camera_temp)
+        # check if Seat is already exist
+        if seat_temp is None:
+            print("Seat doesnt exist")
+        elif camera_temp is None:
+            print("Camera doesnt exist")
+        else:
+            # create camera_data and add it to relate seat
+            c_data = Camera_Data()
+            c_data.camera = Camera.objects.get(name= camera_name)
+            c_data.occupy = occupy
+            c_data.save()
+            print("new Camera data: " + str(c_data))
+
+            #check new camera send data to current seat
+            if seat_temp.camera.count() == 0:
+                seat_temp.camera.add(camera_temp)
+                seat_temp.save()
+                print("new camera added")
+            elif camera_temp not in seat_temp.camera.all():
+                seat_temp.camera.add(camera_temp)
+                seat_temp.save()
+                print("new camera added")
+
+            # add data to current Seat
+            ifnew = False
+            for data in seat_temp.camera_data.all():
+                print("Seat data" + str(data))
+                if data.camera == camera_temp:
+                    ifnew = False
+                    pass
+                else:
+                    ifnew = True
+
+                if ifnew:
+                    seat_temp.camera_data.add(c_data)
+                    seat_temp.save()
+
+            #check full camera data
+            occupy_count = 0
+            if len(seat_temp.camera_data.all()) == len(seat_temp.camera.all()):
+                for data in seat_temp.camera_data.all():
+                    if data.occupy >= "1":
+                        occupy_count += 1
+
+                # majority decision
+                if occupy_count > len(seat_temp.camera.all())/2 :
+                    seat_temp.occupy = True
+                else:
+                    seat_temp.occupy = False
+
+                seat_temp.camera_data.clear()
+                seat_temp.save()
+
+        '''#remove same seat_number
         rows = Seat.objects.filter(seat_number=seat_number)
         for r in rows: 
             r.delete() 
@@ -41,7 +100,7 @@ def create(response):
             s.occupy=True
         else:
             s.occupy=False
-        s.save()    
+        s.save()'''
 
         #count the available seats
         location = Location.objects.get(name=floor)
